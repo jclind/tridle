8d88e20c762beca0984f5f3818c6d1be8efe97ce
Jesse Lind <Jesseclind@gmail.com>
2022-03-31 14:52:01 -0400

    clean: add comments to TridleRow.js

--- src/Components/WordleRow/WordleRow.js
+++ src/Components/WordleRow/WordleRow.js
@@ -20,9 +20,11 @@ const WordleRow = ({
   for (let i = 0; i <= LTRS_IN_WORD - 1; i++) {
     let currLetter = ''
     let currWordPosition = ''
+    // If the current row is selected and currWord[i] exists, currWord[i] is the current letter
     if (isSelected && currWord[i]) {
       currLetter = currWord[i]
     } else if (pastWord) {
+      // If the row is not selected and past word exists, the current letter is the current letter in pastWord at index i
       currLetter = pastWord.words[i].letter
       currWordPosition = pastWord.words[i].position
     }
@@ -37,6 +39,7 @@ const WordleRow = ({
   }
   return (
     <div
+      // If the current word is not valid, the current row is selected, and current word length is equal to LTRS_IN_WORD, turn text red
       className={
         !currWordValid && isSelected && currWord.length === LTRS_IN_WORD
           ? 'wordle-row invalid'
