c6290ecd201cb67b0d31eab1e7f2a081b23b3d0e
Jesse Lind <Jesseclind@gmail.com>
2022-04-01 12:52:17 -0400

    feat: add google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App'
 import 'normalize.css'
 import './index.scss'
 import './themes.scss'
+import ReactGA from 'react-ga'
+const TRACKING_ID = 'G-XKBYT553LJ'
+ReactGA.initialize(TRACKING_ID)
 
 ReactDOM.render(
   <React.StrictMode>
